0c0717b398262605aafe6f7ce78d7b12124633be
emerson <emerson91030@gmail.com>
2016-08-10 23:48:23 -0700

    Add routing

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 const meetUp = angular.module("meetUp", ["ngRoute"]);
 
+meetUp.config(function($routeProvider) {
+	$routeProvider
+	.when("/", {
+		templateUrl: "pages/welcome.html",
+		controller: "mainCtrl"
+	})
+	.when("/createaccount", {
+		templateUrl: "pages/createaccount.html",
+		controller: "mainCtrl"
+	})
+	.when("/createevent", {
+		templateUrl: "pages/createevent.html",
+		controller: "mainCtrl"
+	})
+});	
+
 meetUp.controller("mainCtrl", ["$scope", "$route", function($scope, $route) {
 	$scope.page = "home";
 	console.log($scope.page);
